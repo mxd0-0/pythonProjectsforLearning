54140b3cd168004bf1e7bdf14e9887b5da49940d
Mohammed <mo7amxd.7@gmail.com>
2024-10-15 15:40:18 +0100

    password generator

---
+++ new.py
@@ -0,0 +1,16 @@
+import random
+
+
+def passwordGenerator(length=12):
+    char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?"
+    password = ''.join(random.choice(char) for _ in range(length))
+    return password
+print("give the length of your password: ")
+b= int(input())
+print("how many password you want?")
+c= int(input())
+for i in range(c):
+    print(passwordGenerator(b))
+
+
+
